bb12d80348cdad6c3ac9a79327fb6313470ba33b
YulyaSim <152449597+YulyaSim@users.noreply.github.com>
2024-04-24 11:35:36 +1100

    HW
    HW

---
+++ PracticalWork19/PracticalWork19.cpp
@@ -0,0 +1,56 @@
+﻿// PracticalWork19.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <string>
+using namespace std;
+class Animal
+{
+public:
+    virtual void Voice()
+    {}
+};
+
+class Dog :public Animal
+{
+public:
+    void Voice() override
+    {
+        cout << "Dog " << "speak WOOF-WOOF" << endl;
+    }
+};
+
+   class Cat :public Animal
+   {
+    public:
+        void Voice() override
+        {
+            cout << "Cat "<< "speak MEOW-MEOW" << endl;
+        }
+    };
+   class Owl :public Animal
+  {
+   public:
+       void Voice() override
+       {
+           cout << "Owl " << "speak WHOOO-WHOOO" << endl;
+       }
+  };
+
+
+    int main()
+    {
+       
+        Animal * newAnimal[3];
+        newAnimal[0] = new Dog();
+        newAnimal[1] = new Cat();
+        newAnimal[2] = new Owl();
+     
+        for (Animal* a : newAnimal)
+        {
+            a->Voice();
+        }
+        return 0;
+    };
+
+
